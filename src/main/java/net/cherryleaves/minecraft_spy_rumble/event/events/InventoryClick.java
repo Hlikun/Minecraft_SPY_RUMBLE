7e165eac5e40af4bf7cf8aece8c1a5f63e8f8d83
ひーくん <98318542+Hlikun@users.noreply.github.com>
2024-04-18 00:21:11 +0900

    ifをswitchに変更

--- src/main/java/net/cherryleaves/minecraft_spy_rumble/event/events/InventoryClick.java
+++ src/main/java/net/cherryleaves/minecraft_spy_rumble/event/events/InventoryClick.java
@@ -4,7 +4,6 @@ import net.cherryleaves.minecraft_spy_rumble.Game;
 import net.cherryleaves.minecraft_spy_rumble.Minecraft_SPY_RUMBLE;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
@@ -14,66 +13,70 @@ import org.bukkit.inventory.ItemStack;
 
 public class InventoryClick {
     public InventoryClick(InventoryClickEvent e) {
-        if (e.getWhoClicked() instanceof Player) {
-            Player player = (Player) e.getWhoClicked();
+        if (e.getWhoClicked() instanceof Player p) {
             // クリックされたGUIを取得する
-            Inventory clickedInventory = e.getClickedInventory();
-            if (clickedInventory == Minecraft_SPY_RUMBLE.SettingGUI) {
+            Inventory gui = e.getClickedInventory();
+
+            if (gui == Minecraft_SPY_RUMBLE.SettingGUI) {
+                e.setCancelled(true);
+
+                // クリックしたアイテムを取得&null回避
                 ItemStack clickedItem = e.getCurrentItem();
-                if (clickedItem != null && clickedItem.getType().equals(Material.RED_CANDLE)) {
-                    Minecraft_SPY_RUMBLE.BWPCount += 1;
-                    player.playSound(player.getLocation(), Sound.BLOCK_DISPENSER_DISPENSE, 1.0f, 1.0f);
-                    Minecraft_SPY_RUMBLE.SettingGUICreate();
-                    player.openInventory(Minecraft_SPY_RUMBLE.SettingGUI);
-                }
-                if (clickedItem != null && clickedItem.getType().equals(Material.BLUE_CANDLE)) {
-                    if (Minecraft_SPY_RUMBLE.BWPCount > 1) {
-                        Minecraft_SPY_RUMBLE.BWPCount -= 1;
-                        player.playSound(player.getLocation(), Sound.BLOCK_DISPENSER_DISPENSE, 1.0f, 1.0f);
+                if (clickedItem == null) return;
+
+                switch (clickedItem.getType()) {
+                    case RED_CANDLE -> {
+                        Minecraft_SPY_RUMBLE.BWPCount += 1;
+                        p.playSound(p.getLocation(), Sound.BLOCK_DISPENSER_DISPENSE, 1.0f, 1.0f);
                         Minecraft_SPY_RUMBLE.SettingGUICreate();
-                        player.openInventory(Minecraft_SPY_RUMBLE.SettingGUI);
+                        p.openInventory(Minecraft_SPY_RUMBLE.SettingGUI);
                     }
-                    else {
-                        player.sendMessage(ChatColor.RED + "人狼の数を1未満にすることはできません");
-                        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_HARP, 1.0f, 0.01f);
-                        e.setCancelled(true);
-                    }
-                }
-                if (clickedItem != null && clickedItem.getType().equals(Material.MAGENTA_CANDLE)) {
-                    Minecraft_SPY_RUMBLE.ParallelTaskCount += 1;
-                    player.playSound(player.getLocation(), Sound.BLOCK_DISPENSER_DISPENSE, 1.0f, 1.0f);
-                    Minecraft_SPY_RUMBLE.SettingGUICreate();
-                    player.openInventory(Minecraft_SPY_RUMBLE.SettingGUI);
-                }
-                if (clickedItem != null && clickedItem.getType().equals(Material.LIGHT_BLUE_CANDLE)) {
-                    if (Minecraft_SPY_RUMBLE.ParallelTaskCount > 0) {
-                        Minecraft_SPY_RUMBLE.ParallelTaskCount -= 1;
-                        player.playSound(player.getLocation(), Sound.BLOCK_DISPENSER_DISPENSE, 1.0f, 1.0f);
-                        Minecraft_SPY_RUMBLE.SettingGUICreate();
-                        player.openInventory(Minecraft_SPY_RUMBLE.SettingGUI);
-                    }
-                    else {
-                        player.sendMessage(ChatColor.RED + "同時に出現するタスクの数を0未満にすることはできません");
-                        player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_HARP, 1.0f, 0.01f);
-                    }
-                }
-                if (clickedItem != null && clickedItem.getType().equals(Material.TOTEM_OF_UNDYING)) {
-                    Minecraft_SPY_RUMBLE.armorStandCount = 0;
-                    for (ArmorStand armorStand : Bukkit.getWorlds().get(0).getEntitiesByClass(ArmorStand.class)) {
-                        if (armorStand.getScoreboardTags().contains("TaskPoint")) {
-                            Minecraft_SPY_RUMBLE.armorStandCount++;
+                    case BLUE_CANDLE -> {
+                        if (Minecraft_SPY_RUMBLE.BWPCount > 1) {
+                            Minecraft_SPY_RUMBLE.BWPCount -= 1;
+                            p.playSound(p.getLocation(), Sound.BLOCK_DISPENSER_DISPENSE, 1.0f, 1.0f);
+                            Minecraft_SPY_RUMBLE.SettingGUICreate();
+                            p.openInventory(Minecraft_SPY_RUMBLE.SettingGUI);
+                        } else {
+                            p.sendMessage(ChatColor.RED + "人狼の数を1未満にすることはできません");
+                            p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HARP, 1.0f, 0.01f);
+                            e.setCancelled(true);
                         }
                     }
-                    if(Minecraft_SPY_RUMBLE.armorStandCount >= Minecraft_SPY_RUMBLE.ParallelTaskCount) {
-                        player.closeInventory();
-                        new Game().Start();
+                    case MAGENTA_CANDLE -> {
+                        Minecraft_SPY_RUMBLE.ParallelTaskCount += 1;
+                        p.playSound(p.getLocation(), Sound.BLOCK_DISPENSER_DISPENSE, 1.0f, 1.0f);
+                        Minecraft_SPY_RUMBLE.SettingGUICreate();
+                        p.openInventory(Minecraft_SPY_RUMBLE.SettingGUI);
                     }
-                    else{
-                        player.sendMessage(ChatColor.RED + "設置してあるアーマースタンドの数を超えて設定することはできません。");
-                        player.playSound(player, Sound.BLOCK_NOTE_BLOCK_BASS, 1f, 1f);
+                    case LIGHT_BLUE_CANDLE -> {
+                        if (Minecraft_SPY_RUMBLE.ParallelTaskCount > 0) {
+                            Minecraft_SPY_RUMBLE.ParallelTaskCount -= 1;
+                            p.playSound(p.getLocation(), Sound.BLOCK_DISPENSER_DISPENSE, 1.0f, 1.0f);
+                            Minecraft_SPY_RUMBLE.SettingGUICreate();
+                            p.openInventory(Minecraft_SPY_RUMBLE.SettingGUI);
+                        } else {
+                            p.sendMessage(ChatColor.RED + "同時に出現するタスクの数を0未満にすることはできません");
+                            p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_HARP, 1.0f, 0.01f);
+                        }
+                    }
+                    case TOTEM_OF_UNDYING -> {
+                        Minecraft_SPY_RUMBLE.armorStandCount = 0;
+                        for (ArmorStand armorStand : Bukkit.getWorlds().get(0).getEntitiesByClass(ArmorStand.class)) {
+                            if (armorStand.getScoreboardTags().contains("TaskPoint")) {
+                                Minecraft_SPY_RUMBLE.armorStandCount++;
+                            }
+                        }
+                        if (Minecraft_SPY_RUMBLE.armorStandCount >= Minecraft_SPY_RUMBLE.ParallelTaskCount) {
+                            p.closeInventory();
+                            new Game().Start();
+                        } else {
+                            p.sendMessage(ChatColor.RED + "設置してあるアーマースタンドの数を超えて設定することはできません。");
+                            p.playSound(p, Sound.BLOCK_NOTE_BLOCK_BASS, 1f, 1f);
+                        }
                     }
                 }
-                e.setCancelled(true);
+
             }
         }
     }
